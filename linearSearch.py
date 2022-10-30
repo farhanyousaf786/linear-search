294b34fb0d995c8c2458a372241770857ee3bf37
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-10-30 15:20:51 -0400

    added all algo of linear search in ui mode

--- linearSearch.py
+++ linearSearch.py
@@ -35,13 +35,55 @@ class LinearSearch(QMainWindow):
 		self.label_list = []
 
 		# desired value
-		self.desired = 11
+		self.desired = 8
 
 		# counter for keeping index
 		self.counter = 0
 
 		# local counter
 		c = 0
+
+				# creating push button to start the search
+		self.search_button = QPushButton("Start Search", self)
+
+		# setting geometry of the button
+		self.search_button.setGeometry(100, 270, 100, 30)
+
+		# adding action to the search button
+		self.search_button.clicked.connect(self.search_action)
+
+		# creating push button to pause the search
+		pause_button = QPushButton("Pause", self)
+
+		# setting geometry of the button
+		pause_button.setGeometry(100, 320, 100, 30)
+
+		# adding action to the search button
+		pause_button.clicked.connect(self.pause_action)
+
+		# creating label to show the result
+		self.result = QLabel("To search : " + str(self.desired), self)
+
+		# setting geometry
+		self.result.setGeometry(350, 280, 200, 40)
+
+		# setting style sheet
+		self.result.setStyleSheet("border : 3px solid black;")
+
+		# adding font
+		self.result.setFont(QFont('Times', 10))
+
+		# setting alignment
+		self.result.setAlignment(Qt.AlignCenter)
+
+		# creating a timer object
+		timer = QTimer(self)
+
+		# adding action to timer
+		timer.timeout.connect(self.showTime)
+
+		# update the timer every 200 millisecond
+		timer.start(200)
 
 		# iterating list of numbers
 		for i in self.number:
@@ -64,6 +106,70 @@ class LinearSearch(QMainWindow):
 
 			# incrementing local counter
 			c = c + 1
+
+
+
+
+	# method called by timer
+	def showTime(self):
+
+		# checking if flag is true
+		if self.start:
+			# linear search
+
+			# checking if the element is equal to desired element
+			if self.label_list[self.counter].text() == str(self.desired):
+
+				# make its color green
+				self.label_list[self.counter].setStyleSheet(
+								"border : 1px solid black; background : lightgreen;")
+
+				# show result in result label
+				self.result.setText("Found at index : "
+									+ str(self.counter))
+
+				# make the start flag false
+				self.start = False
+
+				# reseting the counter
+				self.counter = 0
+
+			# if element is not equal
+			else:
+
+				# make the label color grey
+				self.label_list[self.counter].setStyleSheet("border : 1px solid black; background : grey;")
+
+			# increment the counter
+			self.counter += 1
+
+			# if counter value become equal to list length
+			if self.counter == len(self.label_list):
+
+				# make start flag false
+				self.start = False
+
+				# show result i.e not found
+				self.result.setText("Not Found")
+
+	# method called by search button
+	def search_action(self):
+
+		# making flag true
+		self.start = True
+
+		# showing text in result label
+		self.result.setText("Started searching...")
+
+	# method called by pause button
+	def pause_action(self):
+
+		# making flag false
+		self.start = False
+
+		# showing text in result label
+		self.result.setText("Paused")
+
 
 
 
